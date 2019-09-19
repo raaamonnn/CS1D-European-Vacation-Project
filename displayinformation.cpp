d4113c00c58b0cd46120488f511da45bd6b068f2
raaamonnn <46769373+raaamonnn@users.noreply.github.com>
2019-09-18 19:08:51 -0700

    Add files via upload

--- displayinformation.cpp
+++ displayinformation.cpp
@@ -1,7 +1,5 @@
 #include "displayinformation.h"
 #include "ui_displayinformation.h"
-#include <QtDebug>
-#include <QSqlRecord>
 
 
 displayInformation::displayInformation(QWidget *parent) :
@@ -18,38 +16,17 @@ displayInformation::~displayInformation()
 
 void displayInformation::on_loadData_clicked()
 {
-    databaseManager->connect();
+    //db.opendb();
 
-    if (!databaseManager->getDatabase().isOpen()) {
-        qDebug() << "Error: connection with database fail";
-        return;
-    }
-        QSqlQuery* query = new QSqlQuery(databaseManager->getPath());
+        QSqlQuery* query = new QSqlQuery( "DATABASE");
 
-        qDebug() << databaseManager->getPath();
         QSqlQueryModel *modal = new QSqlQueryModel();
-        query->prepare("SELECT * From Amsterdam");
+        query->prepare("SELECT * FROM Berlin");
         query->exec();
 
         modal->setQuery(*query);
 
-//        ui->table->setEditTriggers(QAbstractItemView::NoEditTriggers);
-//        modal->setHeaderData(0, Qt::Horizontal, QObject::tr("Place"));
-//        modal->setHeaderData(1, Qt::Horizontal, QObject::tr("Food"));
-
-//        int i;
-//        QSqlRecord row;
-//        for(i = 0, row = modal->record(i); !row.isEmpty(); i++, row = modal->record(i))
-//        {
-//           modal->insertRow(i);
-//           //How do u Do this ???
-
-//            // Get each field using `value` method of variabale 'row'
-//            // and insert this fields to its corresponding cell in QTableWidget
-//        }
-        qDebug() << (modal->rowCount());
-
         ui->table->setModel(modal);
 
-        //close database conection
+        //db.closedb();
 }
--- displayinformation.h
+++ displayinformation.h
@@ -2,10 +2,7 @@
 #define DISPLAYINFORMATION_H
 
 #include <QDialog>
-#include <QtSql/QSqlQuery>
-#include <QtSql/QSqlQueryModel>
-#include <databasemanager.h>
-
+#include <QtSql>
 namespace Ui {
 class displayInformation;
 }
@@ -15,21 +12,41 @@ class displayInformation : public QDialog
     Q_OBJECT
 
 public:
-    explicit displayInformation(QWidget *parent = nullptr);
-    displayInformation(DatabaseManager *databaseManager)
-    {
-        this->databaseManager = databaseManager;
+    QSqlDatabase mydb;
+
+    bool opendb(){
+    QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
+    mydb.setDatabaseName("//put database name here");
+
+    if(!mydb.open()){
+        qDebug() << "error opening database\n";
+        return false;
     }
 
+    else{
+        qDebug() << "database opened\n";
+        return true;
+    }
+
+    }
+
+    void closedb(){
+        mydb.close();
+        mydb.removeDatabase(QSqlDatabase::defaultConnection);
+    }
+
+    explicit displayInformation(QWidget *parent = nullptr);
     ~displayInformation();
 
 private slots:
+    void on_loadCities_clicked();
+
+    void on_loadFood_clicked();
 
     void on_loadData_clicked();
 
 private:
     Ui::displayInformation *ui;
-    DatabaseManager *databaseManager;
 };
 
 #endif // DISPLAYINFORMATION_H
