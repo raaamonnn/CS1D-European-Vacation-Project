2f00a32f895ec0268bebb74dae460f516ec71460
zjanda <33186496+zjanda@users.noreply.github.com>
2019-09-23 16:41:59 -0700

    Add files via upload
    Work in progress...

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-//    const QString PATH = "my path"; //my path
-
-//    DatabaseManager *databaseManager = new DatabaseManager(PATH);
-//    databaseManager->connect();
     MainWindow w;
-
-
-
-    w.showMaximized();
-
+    w.show();
     return a.exec();
 }
