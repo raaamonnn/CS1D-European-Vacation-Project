a10ab6e00861fefa5119c784e6fb74b749257e5b
Poutin <46769413+Poutin@users.noreply.github.com>
2019-09-23 16:39:03 -0700

    Code format for connecting to database

---
+++ European_Vacation/displayinformation.h
@@ -0,0 +1,45 @@
+#ifndef DISPLAYINFORMATION_H
+#define DISPLAYINFORMATION_H
+
+#include <QDialog>
+#include<QtSql>
+#include<QDebug>
+#include<QFileInfo>
+
+namespace Ui {
+class displayInformation;
+}
+
+class displayInformation : public QDialog
+{
+    Q_OBJECT
+
+public:
+    QSqlDatabase mydb;
+    void connClose(){
+        mydb.close();
+        mydb.removeDatabase(QSqlDatabase::defaultConnection);
+    }
+    bool connOpen(){
+        mydb = QSqlDatabase::addDatabase("QSQLITE");
+        mydb.setDatabaseName("C:\\Users\\mrpou\\Desktop\\European Vacation.db");    //connects to the database
+
+        if(mydb.open()){
+            qDebug("DB connection succesful");
+            return true;
+        }
+        else{
+            qDebug("Failed to connect to DB");
+            return false;}
+        }
+    explicit displayInformation(QWidget *parent = nullptr);
+    ~displayInformation();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::displayInformation *ui;
+};
+
+#endif // DISPLAYINFORMATION_H
