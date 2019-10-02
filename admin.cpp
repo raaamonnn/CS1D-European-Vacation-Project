6bc766264a8e30473fc23573c4ea919593cde838
Poutin <46769413+Poutin@users.noreply.github.com>
2019-10-01 18:49:57 -0700

    Working Administrator Functions

--- admin.cpp
+++ admin.cpp
@@ -6,6 +6,7 @@ admin::admin(QWidget *parent) :
     ui(new Ui::admin)
 {
     ui->setupUi(this);
+
 }
 
 admin::~admin()
@@ -13,10 +14,77 @@ admin::~admin()
     delete ui;
 }
 
-void admin::on_login_clicked()
+void admin::on_comboBox_activated(const QString &arg1)
 {
-    //database stuff
-    //if login correct then show next screen
-    destroy();
-    adminView.show();
+
+   QString city = arg1;
+
+   admin conn;
+   QSqlQueryModel* model = new QSqlQueryModel();
+   conn.connOpen();
+   QSqlQuery* qry = new QSqlQuery(conn.mydb);
+
+   qry->prepare("select * from '"+city+"'");
+   qry->exec();
+
+   model->setQuery(*qry);
+   ui->tableView->setModel(model);
+
+   conn.connClose();
+}
+
+void admin::on_pushButton_clicked() //adding a new food item
+{
+    QString food, price, table;
+    table = ui->comboBox->itemText(ui->comboBox->currentIndex());
+    food = ui->lineEdit->text();
+    price = ui->lineEdit_2->text();
+
+    admin db;
+    db.connOpen();
+    QSqlQuery query;
+    query.prepare("insert into '"+table+"' (Food, Price) values ('"+food+"','"+price+"')");
+
+    if(query.exec()){
+        qDebug()<<"Changes Made";
+        db.connClose();
+    }
+    else{qDebug()<<"Changes failed";
+    query.lastError();}
+    db.connClose();
+}
+
+void admin::on_pushButton_3_clicked() //change price of food item
+{
+    QString food, new_price, table;
+    food = ui->lineEdit_4->text();
+    new_price = ui->lineEdit_5->text();
+    table = ui->comboBox->itemText(ui->comboBox->currentIndex());
+
+    admin db;
+    db.connOpen();
+    QSqlQuery query;
+    query.prepare("update '"+table+"' set Price='"+new_price+"' where Food = '"+food+"'");
+    query.exec();
+    db.connClose();
+}
+
+void admin::on_pushButton_2_clicked() //remove a food item entirely
+{
+    QString food, table;
+    food = ui->lineEdit_3->text();
+    table = ui->comboBox->itemText(ui->comboBox->currentIndex());
+
+    admin db;
+    db.connOpen();
+    QSqlQuery query;
+    query.prepare("delete from '"+table+"' where Food = '"+food+"'");
+    if (query.exec())
+    {
+        std::cout << "query success";
+    }
+    else {
+        std::cout << "query failed";
+    }
+    db.connClose();
 }
